d58216ba09629c9a8496be7b8f17833b8a183496
Ainhoa Lopez <ainhoalv92@gmail.com>
2022-11-24 11:18:17 +0100

    feat(): Ejemplo navegacion

---
+++ App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import {LogIn} from './LogIn';
+import {NavigationTab} from './NavigationTab'; 
+
+const Stack = createStackNavigator();
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator options="false">
+      <Stack.Screen name="Log In" component={LogIn} />
+      <Stack.Screen name="NavigationTab" component={NavigationTab} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
